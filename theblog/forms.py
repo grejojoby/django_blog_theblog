7b8cb3568a69e1fd490d7839c463be15b5471c85
Grejo Joby <grejo00@gmail.com>
2021-03-26 22:52:19 +0530

    categories

--- theblog/forms.py
+++ theblog/forms.py
@@ -1,27 +1,30 @@
 from django import forms
 # from django.forms import fields, widgets
-from .models import Post
+from .models import Post, Category
+
+choices = Category.objects.all().values_list('name','name')
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'body')
+        fields = ('title', 'author', 'body','category')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choices,attrs={'class': 'form-control'}),
 
         }
 
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'body')
+        fields = ('title', 'body', 'category')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
 
         }
--- theblog/views.py
+++ theblog/views.py
@@ -3,7 +3,7 @@ from theblog.models import Post
 from typing import List
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import EditForm, PostForm
 
 # Create your views here.
@@ -15,6 +15,10 @@ class HomeView(ListView):
     template_name = 'home.html'
     ordering = ['-post_date']
 
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats)
+    return render(request, 'categories.html',{'cats':cats, 'category_posts':category_posts})
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
@@ -24,6 +28,14 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
     # fields = '__all__'
+
+
+class AddCategoryView(CreateView):
+    model = Category
+    template_name = 'add_category.html'
+    fields = '__all__'
+
+
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
