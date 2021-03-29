b8d51c845fa6a2693e895f947e78e6ee86d9912e
Grejo Joby <grejo00@gmail.com>
2021-03-29 14:31:12 +0530

    post author fix

--- theblog/forms.py
+++ theblog/forms.py
@@ -12,8 +12,8 @@ class PostForm(forms.ModelForm):
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            # 'author': forms.TextInput(attrs={'class': 'form-control', 'id':'author'}),
-            'author': forms.Select(attrs={'class': 'form-control', "id":"author", '':''}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'id':'author', 'type':'hidden'}),
+            # 'author': forms.Select(attrs={'class': 'form-control', "id":"author", '':''}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             'category': forms.Select(choices=choices,attrs={'class': 'form-control'}),
 
