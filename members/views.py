4f301c360a508ff0b231c2081ed3eeb59dcc03ed
Grejo Joby <grejo00@gmail.com>
2021-03-29 13:27:24 +0530

    REgistration form revamp

--- members/views.py
+++ members/views.py
@@ -1,3 +1,4 @@
+from members.forms import SignUpForm
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
@@ -6,6 +7,6 @@ from django.urls import reverse_lazy
 # Create your views here.
 
 class UserRegistrationView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
